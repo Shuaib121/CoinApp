2d858e202294c428b29ba97d4b67bed4d9c031b3
Shuaib Ebrahim <ebrahimshuaib1@gmail.com>
2021-09-21 21:17:16 +0200

    Added models which use required interfaces.

---
+++ Models/Coin.cs
@@ -0,0 +1,19 @@
+﻿namespace CoinJarApp.Models
+{
+    using CoinJarApp.Interfaces;
+
+    class Coin : ICoin
+    {
+        private decimal _amount;
+        private decimal _volume;
+
+        public Coin(decimal amount, decimal volume)
+        {
+            _amount = amount;
+            _volume = volume;
+        }
+
+        public decimal Amount { get => _amount; set => _amount = value; }
+        public decimal Volume { get => _volume; set => _volume = value; }
+    }
+}
---
+++ Models/CoinJar.cs
@@ -0,0 +1,32 @@
+﻿namespace CoinJarApp.Models
+{
+    using CoinJarApp.Interfaces;
+
+    class CoinJar : ICoinJar
+    {
+        private decimal totalAmount = 0;
+        private decimal totalVolume = 0;
+
+        public void AddCoin(ICoin coin)
+        {
+            totalAmount += coin.Amount;
+            totalVolume += coin.Volume;
+        }
+
+        public decimal GetTotalAmount()
+        {
+            return totalAmount;
+        }
+
+        public decimal GetTotalVolume()
+        {
+            return totalVolume;
+        }
+
+        public void Reset()
+        {
+            totalAmount = 0;
+            totalVolume = 0;
+        }
+    }
+}
